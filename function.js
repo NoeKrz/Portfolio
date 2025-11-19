00b5429cce697cbbcd0e13b885fe5be726cc6360
Salapille <noecool@outlook.fr>
2025-11-19 21:15:59 +0100

    Initial commit

---
+++ function.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll(".project-section");
+const navLinks = document.querySelectorAll(".side-nav a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - window.innerHeight / 2) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
